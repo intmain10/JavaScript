35caf258952856aeb098cf58bd1a3b63594e39cf
intmain10 <100391857+intmain10@users.noreply.github.com>
2023-12-17 12:15:48 +0000

    Datatypes conversion

---
+++ javascript/data_type_conversion.js
@@ -0,0 +1,16 @@
+let score = true
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//true =>1 ,false =0
+
+
+let isLoggedIn =null
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
