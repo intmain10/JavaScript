a904b0161c2074dd26e6fca74d278294578618dc
intmain10 <100391857+intmain10@users.noreply.github.com>
2023-12-17 12:42:40 +0000

    add more

--- javascript/data_type_conversion.js
+++ javascript/data_type_conversion.js
@@ -1,16 +1,45 @@
-let score = true
-console.log(typeof score);
+// let score = true
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
-// "33" => 33
-// "33abc" => NaN
-//true =>1 ,false =0
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-let isLoggedIn =null
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// // "33" => 33
+// // "33abc" => NaN
+// //true =>1 ,false =0
+
+
+// let isLoggedIn =null
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//****************************Operation *********
+
+let value =3;
+let negValue =-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2**2);
+// console.log(2/2);
+
+// let str1="Hello"
+// let str2="Shushant"
+// let str3= str1 + str2
+// console.log(str3);
+
+// console.log(+true);
+// console.log(+"");
+// let num1, num2, num3
+// num1 =num2 =num3 =2+2
+// console.log(num1);
+
+let gameCounter=100;
+gameCounter++;
+--gameCounter;
+console.log(gameCounter);
