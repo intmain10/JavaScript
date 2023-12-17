fe1feea9bc8791cf7faf5f2966aeb19b15f5f88b
intmain10 <100391857+intmain10@users.noreply.github.com>
2023-12-17 11:57:56 +0000

    Datatypes of JavaScript

---
+++ javascript/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert("Hello")  // we are using nodejs not browser
+
+// console.log();
+
+let name="Shushant"
+let age =21
+let isLoggedIn =false
+let state =null;
+
+
+//number => 2 to power 53
+//bigint
+//strinf => ""
+//boolean => true/false
+// null=> standalone value
+// undefined => 
+// symbol => unique 
+
+//object 
+
+console.log(typeof undefined);
